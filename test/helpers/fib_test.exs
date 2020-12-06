f2b55eb8444d6e1c992441c8de5c95dd29f4b0bc
Daniel Salazar <disalazarg@gmail.com>
2020-12-06 08:07:58 -0300

    PropCheck integration for helper modules

--- test/helpers/fib_test.exs
+++ test/helpers/fib_test.exs
@@ -1,13 +1,34 @@
 defmodule Euler.Helpers.FibTest do
   use ExUnit.Case
+  use PropCheck, default_opts: [numtests: 100]
 
   alias Euler.Helpers.Fib
 
   describe "the fibonacci generator" do
     test "works as a stream" do
-      fibs = Fib.stream() |> Stream.take(5) |> Enum.to_list()
+      fibs =
+        Fib.stream()
+        |> Stream.take(5)
+        |> Enum.to_list()
 
       assert fibs == [1, 1, 2, 3, 5]
     end
+
+    property "has the nth number in the series the same as a reference model" do
+      forall num <- integer(1, 10_000_000) do
+        nth =
+          Fib.stream()
+          |> Stream.take(num)
+          |> Enum.to_list()
+          |> List.last()
+
+        nth == fib(num, 1, 0)
+      end
+    end
   end
+
+  # Reference model, tail-recursive base definition for a = 1, b = 0
+  defp fib(n, a, b)
+  defp fib(1, a, _b), do: a
+  defp fib(n, a, b), do: fib(n - 1, a + b, a)
 end
--- test/helpers/prime_test.exs
+++ test/helpers/prime_test.exs
@@ -1,16 +1,27 @@
 defmodule Euler.Helpers.PrimeTest do
   use ExUnit.Case
+  use PropCheck, default_opts: [numtests: 100]
 
   alias Euler.Helpers.Prime
 
-  describe "the prime decomposition" do
-    test "when multiplied, returns the original number" do
-      result =
-        13195
+  describe "a prime decomposition" do
+    property "has all members being themselves primes" do
+      forall num <- integer(2, 10_000_000) do
+        num
         |> Prime.factors()
-        |> Enum.reduce(&*/2)
+        |> Enum.all?(fn n -> Prime.factors(n) == [n] end)
+      end
+    end
 
-      assert result == 13195
+    property "when multiplied, returns the original number" do
+      forall num <- integer(2, 10_000_000) do
+        result =
+          num
+          |> Prime.factors()
+          |> Enum.reduce(&*/2)
+
+        result == num
+      end
     end
   end
 end
